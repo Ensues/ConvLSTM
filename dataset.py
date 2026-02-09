6da7b7b9fd1f2fcfc9ce25a281e390e7cadbe9aa
404hannah <hannah.pulmano865@gmail.com>
2026-02-09 14:40:56 +0800

    label

--- dataset.py
+++ dataset.py
@@ -48,13 +48,25 @@ class MVOVideoDataset(Dataset):
         csv_path = os.path.join(self.label_folder, csv_name)
         
         # Read the label
-        # For a 3s clip, the label is the "majority" action or the final state
+        # For a 3s clip, the label is the maximum turn label present in the last second (24 frames in CSV)
         df = pd.read_csv(csv_path)
-        label = df['label'].iloc[-1] # Taking the final decision of the MVO
-        
+        df_lbl_count = []
+        for i in range(0, 3):
+            df_lbl_count = df['label'].tail(24).count(i)
+
+        if df_lbl_count[0] == 24:
+            label = 0 # Front
+        elif df_lbl_count[1] > df_lbl_count[2]:
+            label = 1 # Left
+        elif df_lbl_count[1] < df_lbl_count[2]:
+            label = 2 # Right
+        else: # If turn counts are equal
+            label = df['label'].tail(12).mode()[0]
+            
         """
         Hannah check this plz
         
+        label = df['label'].iloc[-1] # Taking the final decision of the MVO
         last_second = df['label'].tail(10)
         label = last_second.mode()[0]
         """
